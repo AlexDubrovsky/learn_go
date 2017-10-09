cb4b8989514755fcb5ee67bce325f04a5c306a5d
Alex Dubrovsky <ad.12379@gmail.com>
2017-10-10 01:43:43 +0300

    first commit

---
+++ helloworld/helloworld.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+	n, err := fmt.Println("1. Hello world!")
+	fmt.Println("2. Hello world!")
+	fmt.Println("3. Hello world!")
+
+	fmt.Println("n = ", n)
+	fmt.Println("err = ", err)
+	fmt.Println("type of err is ", reflect.TypeOf(err))
+}
