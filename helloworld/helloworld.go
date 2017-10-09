1669486199b5616eba043159684ef0e12d441fda
Alex Dubrovsky <ad.12379@gmail.com>
2017-10-10 01:53:12 +0300

    comment

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -10,6 +10,7 @@ func main() {
 	n, err := fmt.Println("1. Hello world!")
 	fmt.Println("2. Hello world!")
 	fmt.Println("3. Hello world!")
+	fmt.Println("4. Hello git!")
 
 	fmt.Println("n = ", n)
 	fmt.Println("err = ", err)
